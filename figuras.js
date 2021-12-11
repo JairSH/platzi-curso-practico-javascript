ded786d698eab53ac02d49ebef33bdb7cb372982
jair <jair@hotmail.com>
2021-12-11 17:59:12 -0600

    se crea ejemplos de declaracion de const y operaciones

--- figuras.js
+++ figuras.js
@@ -1,1 +1,10 @@
-console.log('Hello World')
+// Codigo del Cuadrado
+
+const ladoCuadrado = 5
+console.log('Los lados del cuadrado miden: ' + ladoCuadrado + 'cm')
+
+const perimetroCuadrado = ladoCuadrado * 4
+console.log('El perimetro del cuadrado es igual: ' + perimetroCuadrado)
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado
+console.log('El perimetro del cuadrado es igual: ' + areaCuadrado)
