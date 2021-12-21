f56148a801b3c1a3ed5cc6ff7311bd583af87575
jair <jair@hotmail.com>
2021-12-21 00:48:57 -0600

    Se realiza modificacion a funcion promedio y se utiliza metodo reduce en lugar de for

--- promedio.js
+++ promedio.js
@@ -1,10 +1,16 @@
 
 function promedio(lista) {
-  let sumaLista = 0
+  /* let sumaLista = 0
 
   for (let i = 0; i < lista.length; i++) {
     sumaLista = sumaLista + lista[i]
-  }
+  } */
+
+  const sumaLista = lista.reduce(
+    function (valorAcumulado = 0, nuevoElemento) {
+      return valorAcumulado + nuevoElemento
+    }
+  )
 
   const promedio = sumaLista / lista.length
   return promedio
