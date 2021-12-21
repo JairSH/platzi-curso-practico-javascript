4b5a07f0a97cca8cf3d2f05718c61497e4cdb6e6
jair <jair@hotmail.com>
2021-12-20 21:17:08 -0600

    Se realiza primer ejemplo de calculo de descuento utilizando switch

--- descuentos.js
+++ descuentos.js
@@ -8,14 +8,34 @@ function calcularPrecioConDescuento (precio, descuento) {
 
 // Calculo de precio con descuento
 
+const arrayDiscount = [
+  '5',
+  '10',
+  '15'
+]
+
 function priceDiscount() {
   const inputPrice = document.getElementById('inputPrice')
   const priceValue = inputPrice.value
 
   const inputDiscount = document.getElementById('inputDiscount')
-  const discountValue = inputDiscount.value
+  const valueDiscount = inputDiscount.value
 
-  const precioTotal = calcularPrecioConDescuento(priceValue, discountValue)
+  let descuento
+
+  switch (valueDiscount) {
+    case arrayDiscount[0]:
+      descuento = 5
+      break
+    case arrayDiscount[1]:
+      descuento = 10
+      break
+    case arrayDiscount[2]:
+      descuento = 15
+      break
+  }
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento)
   const resultP = document.getElementById('resultP')
-  resultP.innerHTML = 'El precio con descuento es de: $' + precioTotal
+  resultP.innerText = precioConDescuento
 }
